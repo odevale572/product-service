8834e64c737e424176fbedb87b6725ebc9c28117
odevale572 <odevale572@gmail.com>
2023-05-14 23:59:03 +0530

    product service changes

--- src/main/java/com/estore/product/ProductServiceApplication.java
+++ src/main/java/com/estore/product/ProductServiceApplication.java
@@ -1,7 +1,9 @@
 package com.estore.product;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,6 +15,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @SpringBootApplication
 //@EnableEurekaClient
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
